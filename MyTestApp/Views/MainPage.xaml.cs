5c5358aa6f86f511e684028b2c8cdcb287ba38bd
alessandro limonta <allelimo@gmail.com>
2020-04-28 13:24:57 +0200

    Add project files.

---
+++ MyTestApp/Views/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml.Controls;
+
+using System.Collections.ObjectModel;
+
+namespace MyTestApp.Views
+{
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            elencostati.Add("Italia");
+            elencostati.Add("Francia");
+            elencostati.Add("Svizzera");
+
+            State.ItemsSource = elencostati;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        ObservableCollection<string> elencostati = new ObservableCollection<string>();
+
+    }
+
+}
---
+++ MyTestApp/Views/SecondPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Windows.UI.Xaml.Controls;
+
+
+namespace MyTestApp.Views
+{
+    public sealed partial class SecondPage : Page, INotifyPropertyChanged
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
